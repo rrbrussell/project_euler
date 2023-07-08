893e6af95c0cb1f5d5f3749d713194496ed8d787
Robert R. Russell <robert@rrbrussell.com>
2023-07-08 10:34:39 -0500

    Problem 5 solved and submitted.

--- problem_0005/src/main.rs
+++ problem_0005/src/main.rs
@@ -1,3 +1,50 @@
+// The prime numbers less than 20 are 2, 3, 5, 7, 11, 13, 17, and 19.
+// I will first try the multiple of all these numbers.
+
 fn main() {
-    println!("Hello, world!");
+    let mut x: u64 = 9699690_u64;
+    loop {
+        x += 2_u64;
+        if x < 9699690_u64 {
+            break;
+        }
+        if x % 20 == 0 {
+            if x % 19 == 0 {
+                if x % 18 == 0 {
+                    if x % 17 == 0 {
+                        if x % 16 == 0 {
+                            if x % 15 == 0 {
+                                if x % 14 == 0 {
+                                    if x % 13 == 0 {
+                                        if x % 12 == 0 {
+                                            if x % 11 == 0 {
+                                                if x % 10 == 0 {
+                                                    if x % 9 == 0 {
+                                                        if x % 8 == 0 {
+                                                            if x % 7 == 0 {
+                                                                if x % 6 == 0 {
+                                                                    if x % 5 == 0 {
+                                                                        if x % 4 == 0 {
+                                                                            if x % 3 == 0 {
+                                                                                break;
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println!("{x}");
 }
