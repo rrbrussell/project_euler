6e9d779691643ef8e36b450aebe37b12bccda448
Robert R. Russell <robert@rrbrussell.com>
2023-07-08 09:32:26 -0500

    Palindrome detector is working.

--- problem_0004/src/main.rs
+++ problem_0004/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn is_palindrome(number: u32) -> bool {
+    let forward: String = number.to_string();
+    let mut backward: String = String::with_capacity(forward.len());
+    let mut temp: String = forward.to_owned();
+    loop {
+        match temp.pop() {
+            Some(ch) => {
+                backward.push(ch);
+            }
+            None => {
+                break;
+            }
+        }
+    }
+    let backward: String = backward;
+    return forward == backward;
+}
+
+#[cfg(test)]
+mod test {
+    use crate::is_palindrome;
+    #[test]
+    fn test_is_palindrome() {
+        let test_data = [
+            (32_u32, false),
+            (99_u32, true),
+            (11_u32, true),
+            (2_u32, true),
+        ];
+        for (test_case, expected_result) in test_data {
+            assert_eq!(is_palindrome(test_case), expected_result);
+        }
+    }
+}
